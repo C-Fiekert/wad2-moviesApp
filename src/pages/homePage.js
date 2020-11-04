9d722e39c674b61661ad373efaaad4f16f4c6a44
Callum Fiekert <fiekertc@gmail.com>
2020-11-04 16:34:53 +0000

    Refactor: Movie list and genre list now stored in contexts

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -3,19 +3,14 @@ import PageTemplate from '../components/templateMovieListPage'
 import {MoviesContext} from '../contexts/moviesContext'
 
 const MovieListPage = () => {
-  const context = useContext(MoviesContext);
-  const movies = context.movies.filter((m) => {  // New
-    return !("favorite" in m);
-  });
+  const context = useContext(MoviesContext) 
 
   return (
-    <PageTemplate
-      title="No. Movies"
-      movies={movies}  /* Changed */
-      action={(movie) => {
-        return <AddToFavoritesButton movie={movie} />;
-      }}
-    />
+      <PageTemplate 
+        title='No. Movies'
+        movies={context.movies}
+        buttonHandler={context.addToFavorites}
+      />
   );
 };
 
